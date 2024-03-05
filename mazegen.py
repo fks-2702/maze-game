48edcb9c5ee1b668790f53a4a1b771720c591a54
fks <f4ferris@gmail.com>
2024-03-04 23:33:18 -0500

    readability

--- mazegen.py
+++ mazegen.py
@@ -7,15 +7,15 @@ def generate_maze(width, height):
 				directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 				#la liste de directions possible
 				random.shuffle(directions)
-				for dx, dy in directions:
+				for direction_x, direction_y in directions:
 						#on ajoute le direction choisie a la position actuelle pour obtenir la nouvelle position 
-						nx, ny = x + dx * 2, y + dy * 2
+						next_x, next_y = x + direction_x * 2, y + direction_y * 2
 						#on verifie que la position est bien dans le labyrinthe
-						if 0 < nx < width * 2 and 0 < ny < height * 2 and maze[ny][nx]:
-								maze[y + dy][x + dx] = 0
-								maze[ny][nx] = 0
+						if 0 < next_x < width * 2 and 0 < next_y < height * 2 and maze[next_y][next_x]:
+								maze[y + direction_y][x + direction_x] = 0
+								maze[next_y][next_x] = 0
 								#on appelle la fonction pour la nouvelle position reccursivement
-								depth_first_search(nx, ny)
+								depth_first_search(next_x, next_y)
         #on genere le labyrinthe
 		depth_first_search(1, 1)
 		maze[1][0] = 0  # entre
@@ -23,13 +23,10 @@ def generate_maze(width, height):
 		return maze
 
 
-def print_maze(maze):
-		for row in maze:
-				print("".join(["#" if cell else " " for cell in row]))
+
 
 #dimension du labyrinthe de default
 width = 10
 height = 10
 #on genere le labyrinthe
 maze = generate_maze(width, height)
-print_maze(maze)
